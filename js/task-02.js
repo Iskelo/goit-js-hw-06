9b0dca07fb7126b7c99aa9fb169b4a99d3ee074e
Iskelo <iskelo.kap@gmail.com>
2023-12-06 20:09:26 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+
+ingredients.forEach(element => {
+  const el = document.createElement('li');
+  el.textContent = element;
+  el.classList.add('item')
+  list.append(el);
+});
