93fb69838bf7335a3919b57b7d7aa2f81796fd51
Iskelo <iskelo.kap@gmail.com>
2023-12-17 15:03:08 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -11,22 +11,9 @@ const ingredients = [
 const list = document.querySelector('#ingredients');
 
 
-
-// ingredients.forEach(element => {
-//   const el = document.createElement('li');
-//   el.textContent = element;
-//   el.classList.add('item')
-//   list.append(el);
-// });
-
-const el = document.createElement('li');
-  
-// const items = list.map(element =>{  
-//   el.textContent = element;
-//   el.classList.add('item')
-//   list.append(el);
-// });
-
-el.classList.add('item')
-list.append(el);
-console.log(list)
+[...ingredients].forEach(element => {
+  const el = document.createElement('li');
+  el.textContent = element;
+  el.classList.add('item')
+  list.append(el);  
+});
