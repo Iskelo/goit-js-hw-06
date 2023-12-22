77ab5d719f361b1fe1671a3794aa4d3bc4da5988
Iskelo <iskelo.kap@gmail.com>
2023-12-22 12:39:13 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const widget = document.querySelector('.widget'),
+color = document.querySelector('.color'),
+changeColor = document.querySelector('.change-color');
+
+
+changeColor.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  widget.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor()
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
