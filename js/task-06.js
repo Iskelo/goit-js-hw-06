aa9244e23e5eb31012177129e6c335839abe325a
Iskelo <iskelo.kap@gmail.com>
2023-12-22 12:29:24 +0100

    task-08

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,19 @@
-/* const symbols = document.querySelector('#validation-input');
+const symbols = document.querySelector('#validation-input');
 const symbolLength = symbols.dataset.length;
 
 
 symbols.addEventListener("blur", (event) => {
-	
-	if(event.currentTarget.value.length +1 > symbolLength){		
+	const inputLength = event.currentTarget.value.length;
+	if(inputLength === parseInt(symbolLength)){		
 		symbols.classList.add('invalid');
 		symbols.classList.remove('valid');
 	}else{		
 		symbols.classList.add('valid');
 		symbols.classList.remove('invalid');
 	}
- }); */
+ });
 
- const symbols = document.querySelector('#validation-input');
+/*  const symbols = document.querySelector('#validation-input');
 const symbolLength = symbols.dataset.length;
 
 symbols.addEventListener("blur", (event) => {
@@ -26,4 +26,4 @@ symbols.addEventListener("blur", (event) => {
     symbols.classList.add('invalid');
     symbols.classList.remove('valid');
   }
-});
+}); */
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit',onSubmit);
+
+function onSubmit(event) {
+	event.preventDefault();
+	const {email, password} = event.currentTarget.elements;
+	if (email.value === '' || password.value === '') {
+		alert('All fields must be filled')
+	}
+	const data ={
+		email: email.value,
+		pasword: password.value
+	}
+	console.log(data)
+	formEl.reset()
+}
 
