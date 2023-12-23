3a2f4a2bd1ea946e46d0261d9c1f143702d74973
Iskelo <iskelo.kap@gmail.com>
2023-12-23 12:34:49 +0100

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,39 +1,36 @@
 const inputElement = document.querySelector('input[type="number"]');
 const destroy = document.querySelector('button[data-destroy]')
 const create = document.querySelector('button[data-create]')
-
 const box = document.querySelector('#boxes');
 
-console.dir(inputElement.value)
-
-
 create.addEventListener('click', onCreate);
+destroy.addEventListener('click', onDestroy)
 
-function onCreate(event) {
-  console.dir(inputElement.value)
+let size = 30;
+let marcup ='';
+let carrentColor = '';
+
+function onCreate() {
+  createBoxes(inputElement.value)
+}
+
+function onDestroy() {
+  marcup ='';  
+  box.innerHTML = marcup;
+  size = 30;
 }
 
 function createBoxes(amount) {
-  
+  for (let i = 0; i < amount; i+=1) {
+    carrentColor = getRandomHexColor();
+    marcup += `<div style="width: ${size}px; height: ${size}px; background-color:${carrentColor};"></div>`;
+    box.innerHTML = marcup;
+    size += 10;
+  }
 }
-
-
-
-
-// destroy.addEventListener('click', onDestroy);
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-
-/* const markup = images.map(({url, alt})=>
-`<li>
-  <img src="${url}" alt="${alt}" class="imgs">
-</li>`
-).join('');
-
-list.insertAdjacentHTML("beforeend", markup); */
