b448c23770c658371e3fbeb68b1f76ac613426ec
Iskelo <iskelo.kap@gmail.com>
2023-12-24 11:28:28 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,11 @@ const symbolLength = symbols.dataset.length;
 
 symbols.addEventListener("blur", (event) => {
 	const inputLength = event.currentTarget.value.length;
-	if(inputLength >= Number(symbolLength)){		
-		symbols.classList.add('invalid');
-		symbols.classList.remove('valid');
-	}else{		
+	if(inputLength === Number(symbolLength)){		
 		symbols.classList.add('valid');
 		symbols.classList.remove('invalid');
+	}else{		
+		symbols.classList.add('invalid');
+		symbols.classList.remove('valid');
 	}
  });
